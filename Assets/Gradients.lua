34c522a112fa8c23a737cb2efded9b6ff971aa3a
teteArchives <talenthuynh6679@gmail.com>
2024-11-11 21:54:28 -0600

    Create Gradients.lua

---
+++ Assets/Gradients.lua
@@ -0,0 +1,22 @@
+return {
+	Cyan = ColorSequence.new({
+		ColorSequenceKeypoint.new(0.00, Color3.fromRGB(87, 216, 255)),
+		ColorSequenceKeypoint.new(1.00, Color3.fromRGB(135, 255, 249)),
+	});
+	Cyan2 = ColorSequence.new({
+		ColorSequenceKeypoint.new(0.00, Color3.fromRGB(55, 195, 255)),
+		ColorSequenceKeypoint.new(1.00, Color3.fromRGB(94, 239, 255)),
+	});
+	Green = ColorSequence.new({
+		ColorSequenceKeypoint.new(0.00, Color3.fromRGB(92, 239, 0)),
+		ColorSequenceKeypoint.new(1.00, Color3.fromRGB(163, 253, 28)),
+	});
+	Red = ColorSequence.new({
+		ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 2, 61)),
+		ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 39, 125)),
+	});
+	Gray = ColorSequence.new({
+		ColorSequenceKeypoint.new(0.00, Color3.fromRGB(147, 149, 168)),
+		ColorSequenceKeypoint.new(1.00, Color3.fromRGB(208, 212, 238)),
+	});
+}
